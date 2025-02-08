006572b800442dd03d75b78534a4cd51a1340365
Henry Corredor <henry.corredor7570@gmail.com>
2025-02-07 21:31:41 -0500

    add component DestinationPage

--- src/pages/DestinationsPage.jsx
+++ src/pages/DestinationsPage.jsx
@@ -1,13 +1,43 @@
-import { Container, Button } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { getDestinations } from '../services/destinationService';
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import { Link } from 'react-router-dom';
 
 const DestinationPage = () => {
+    const [destinations, setDestinations] = useState([]);
+    console.log(destinations)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await getDestinations();
+            setDestinations(data);
+        };
+        fetchData();
+    }, []);
+
     return (
-        <Container className='text-center mt-5'>
-            <h1>Explora tus destinos y libera tus sentidos</h1>
-            <Button variant='primary' as={Link} to='/'>
-                Home
-            </Button>
+        <Container className='mt-4'>
+            <h2 className="text-center">Explora tus destinos y libera tus sentidos</h2>
+            <Row>
+                {destinations.length > 0 ? (
+                    destinations.map((destination) => (
+                        <Col md={4} key={destination.id} className="mb-4">
+                            <Card>
+                                <Card.Img variant="top" src={destination.image_url} alt={destination.name}/>
+                                <Card.Body>
+                                    <Card.Title>{destination.name}</Card.Title>
+                                    <Card.Text>{destination.description.substring(0,100)}...</Card.Text>
+                                    <Button variant="primary" as={Link} to={`/destination/${destination.id}`}>
+                                        Ver Detalles
+                                    </Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))
+                ) : (
+                    <p className="text-center">No hay destinos disponibles</p>
+                )}
+            </Row>
         </Container>
     );
 };
--- src/services/destinationService.js
+++ src/services/destinationService.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:3000/";
+const  API_URL = "http://localhost:3000/destinations"
 
-export const getdestinations = () => {
+export const getDestinations = async () => {
     try {
-        const response = await axios.get(API_URL)
+        const response = await axios.get(API_URL);
+        return response.data;
     } catch (error) {
         console.error("Error al obtener los destinos", error);
         return [];
